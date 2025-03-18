92f3bee76364614a6c1b1368a9df8b21db8a3f3c
hbajraktari <hysnije.bb1@gmail.com>
2025-03-18 08:02:01 -0700

    Inital Commit for Vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>
+    {{ message }}
+  </h1>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const message = ref('Hysnije')
+
+setTimeout(() => {
+  message.value = 'Bajraktari'
+}, 5000)
+</script>
+
+<style>
+h1 {
+  color: rgb(32, 19, 119);
+}
+</style>
